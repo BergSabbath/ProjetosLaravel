6fd1f9a943d4fda6bf83e6352ceff036e60e4565
BergSabbath <bergit7@gmail.com>
2018-11-14 16:40:22 -0300

    criado as categorias

---
+++ cadastro/resources/views/produtos.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app',["current"=>"produtos"])
+
+@section('body')
+    <h1>Pagina de Produtos</h1>
+@endsection
---
+++ cadastro/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/produtos','ControladorProduto@index');
+
+Route::get('/categorias','ControladorCategoria@index');
+//a diferença de passar pelo controlador e não mandar direto para view
+//é que no controlador possui outras funçoes mais completas (index,create, edit, store, show, update, destroy)
+Route::get('/categorias/novo','ControladorCategoria@create');
+
+Route::post('/categorias','ControladorCategoria@store');
+Route::get('/categorias/apagar/{id}','ControladorCategoria@destroy');
+Route::get('/categorias/editar/{id}','ControladorCategoria@edit');
+Route::post('/categorias/{id}','ControladorCategoria@update');
