b4375891775b68b6b19866ba05f58a3e307a8cbf
BergSabbath <bergit7@gmail.com>
2018-11-26 12:12:50 -0300

    atualizando

--- validacao/resources/views/novocliente.blade.php
+++ validacao/resources/views/novocliente.blade.php
@@ -20,23 +20,57 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <form action="/cliente" method="POST">
+                        <form action="/clientes" method="POST">
+                        @csrf
                             <div class="form-group">
                                 <label for="nome">Nome do Cliente</label>
-                                <input type="text" id="nome" class="form-control" name="nome" placeholder="Nome do Cliente">
+                                <input type="text" id="nome" class="form-control {{$errors->has('nome') ? 'is-invalid' : ''}}" name="nome" placeholder="Nome do Cliente">
+            @if($errors->has('nome'))
+                            <div class="invalid-feedback">
+                {{$errors->first('nome')}}
+                            </div>
+            @endif
                             </div>
                             <div class="form-group">
                                 <label for="nome">Idade do Cliente</label>
-                                <input type="text" id="idade" class="form-control" name="idade" placeholder="Idade do Cliente">
+                                <input type="text" id="idade" class="form-control {{$errors->has('idade') ? 'is-invalid' : ''}}" name="idade" placeholder="Idade do Cliente">
+            @if($errors->has('idade'))
+                            <div class="invalid-feedback">
+                {{$errors->first('idade')}}
+                            </div>
+            @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="nome">Endereço do Cliente</label>
+                                <input type="text" id="endereco" class="form-control {{$errors->has('endereco') ? 'is-invalid' : ''}}" name="endereco" placeholder="Endereço do Cliente">
+            @if($errors->has('nome'))
+                            <div class="invalid-feedback">
+                {{$errors->first('endereco')}}
+                            </div>
+            @endif
                             </div>
                             <div class="form-group">
                                 <label for="nome">E-mail do Cliente</label>
-                                <input type="text" id="email" class="form-control" name="email" placeholder="E-mail do Cliente">
+                                <input type="text" id="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" name="email" placeholder="E-mail do Cliente">
+            @if($errors->has('email'))
+                            <div class="invalid-feedback">
+                {{$errors->first('email')}}
+                            </div>
+            @endif
                             </div>
                             <button type="submit" class="btn btn-primary btn sm">Salvar</button>
                             <button type="reset" class="btn btn-danger btn sm">Cancelar</button>
                         </form>
                     </div>
+    <!-- @if($errors->any())            //para aparecer as caixas de texto em baixo com erros
+                <div class="card-footer">                          
+        @foreach($errors->all() as $error)
+                    <div class="alert alert-danger" role="alert">
+                        {{$error}}
+                    </div>
+        @endforeach
+                </div>
+    @endif -->
                 </div>
             </div>
         </div>
